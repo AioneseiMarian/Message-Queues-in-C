f88a958b9898eb8d869ec31f2e6ccd930be0aab4
AioneseiMarian <marianaionesei7@gmail.com>
2025-01-08 11:01:09 +0200

    Done with the repeating messages

--- server/message_send.c
+++ server/message_send.c
@@ -13,19 +13,39 @@ void set_all_messages_as_old(Queue_Node* msg_queue){  //From a specific topic an
     }
 }
 
-void set_all_subs_as_old(HashTable* table){
+void aux_set_subs_old(Queue_Node* sub_queue){
+    Subscribtion* sub;
+    while(sub_queue != NULL)
+    {
+        sub = sub_queue->data;
+        sub->new = 0;
+
+        sub_queue = sub_queue->next_node;
+    }
+}
+
+void set_all_subs_as_old(HashTable* table){     //not thread safe, but used in locked block
     for (int i = 0; i < HASH_TABLE_SIZE; ++i) {
         HashTableEntry* entry = table->buckets[i];
         while (entry) {
-            Queue_Node* sub_queue = entry->tree->root->queue;
-            while(sub_queue != NULL)
-            {
-                Subscribtion* sub = sub_queue->data;
-                sub->new = 0;
-                sub_queue = sub_queue->next_node;
+            Queue_Node* aux_queue = NULL;  // Initialize to NULL as per push_Queue's expectation
+            push_Queue(&aux_queue, (void*)entry->tree->root);
+
+            while (aux_queue != NULL) {  // While queue is not empty
+                RBTNode* tree_node = (RBTNode*)pop_Queue(&aux_queue);
+                if (tree_node && tree_node->queue) {
+                    aux_set_subs_old(tree_node->queue);    //set all subs as old from rbtree node(queue)
+                }
+
+                if(tree_node->left != entry->tree->NIL)
+                    push_Queue(&aux_queue, tree_node->left);
+                
+                if(tree_node->right != entry->tree->NIL)
+                    push_Queue(&aux_queue, tree_node->right);
             }
+            free_Queue(&aux_queue);
             entry = entry->next;
-       }
+        }
     }
 }
 
@@ -43,6 +63,7 @@ void send_message(int client_fd, const char* json_string)
 
 void aux_send_messages_from_queues(Server* server, Queue_Node* msg_queue){      //from a msg queue with specific topic and subtopic
     Message* msg;
+    Queue_Node* aux_queue = msg_queue;
     while(msg_queue != NULL)
     {
         msg = msg_queue->data;
@@ -60,6 +81,7 @@ void aux_send_messages_from_queues(Server* server, Queue_Node* msg_queue){      
             Subscribtion* sub = sub_queue->data;
             if(sub->new == 0){
                 if(msg->header.new == 1){
+                    printf("Sending new message to old subscriber\n");
                     if(sub->has_interest == 1){
                         if(strstr(msg->data, sub->interest) != NULL){
                             send_message(sub->client_fd, json_string);
@@ -70,6 +92,7 @@ void aux_send_messages_from_queues(Server* server, Queue_Node* msg_queue){      
                 }
             }
             else{
+                printf("Sending message to new subscriber\n");
                 if(sub->has_interest == 1){
                     if(strstr(msg->data, sub->interest) != NULL){
                         send_message(sub->client_fd, json_string);
@@ -85,7 +108,7 @@ void aux_send_messages_from_queues(Server* server, Queue_Node* msg_queue){      
 
         msg_queue = msg_queue->next_node;
     }
-    set_all_messages_as_old(msg_queue);     //from this subtopic
+    set_all_messages_as_old(aux_queue);     //from this subtopic
 }
 
 void send_messages_to_subs(Server* server){
@@ -119,6 +142,7 @@ void send_messages_to_subs(Server* server){
         }
     }
     
+    set_all_subs_as_old(server->subscribtions);
 
     pthread_mutex_unlock(&table_mutex);
 }
